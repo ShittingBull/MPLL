bf7c1b18bb2786936fa346df69051c23f628608a
Johannes Böhler <JohannesBoehler@Johanness-MBP.fritz.box>
2016-03-08 19:35:38 +0100

    "Length of pitch and time vector changed"

--- MonoPLLPitchtrack.m
+++ MonoPLLPitchtrack.m
@@ -32,22 +32,21 @@ inpp = in';
 % -----------------------------------------------------------
 
 
-figure;
-clf;
+%figure;
 t = (1:length(inpp)) ./ fs;
 %subplot 212
-BS = 4096;
-plotSpectrogram(inpp, BS, 512, fs, 'ylim', [1 1200]);
-hold on;
-plot(F0)
-plot((1:length(F0)-BS/2+1)./fs, F0(BS/2:end), 'k', 'LineWidth',2);
-hold off;
-timeVectorOut = zeros(1, ceil(length(F0) / (fs/100)));
-pitchOut = zeros(1, ceil(length(F0) / (fs/100)));
+% BS = 4096;
+% plotSpectrogram(inpp, BS, 512, fs, 'ylim', [1 1200]);
+% hold on;
+% plot(F0)
+% plot((1:length(F0)-BS/2+1)./fs, F0(BS/2:end), 'k', 'LineWidth',2);
+% hold off;
+timeVectorOut = zeros(1, floor(length(F0) / (fs/100)));
+pitchOut = zeros(1, floor(length(F0) / (fs/100)));
 %probOut = zeros(1, ceil(length(medTruePitch) / 441));
-for i = 1:fs/100:length(F0)
-    timeVectorOut (((i-1)/(fs/100)) + 1) = (i-1) / fs; 
-    pitchOut (((i-1)/(fs/100)) + 1) = F0(i);
+for i = fs/100:fs/100:length(F0)
+    timeVectorOut (i/(fs/100)) = i / fs; 
+    pitchOut (i/(fs/100)) = F0(i);
     %probOut (((i-1)/441) + 1) = prob(i);
     %fprintf(fileID,'%.3f \t %.3f \t %.3f \n', (i-1)/11000 , medTruePitch(i), prob(i));
 end
